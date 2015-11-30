233eca563422dee20d131049dade0ac7b28fe63d
Eric Wieser <wieser.eric@gmail.com>
2015-11-30 00:52:54 -0500

    Add parameter estimates for real system
    Rename functions
    
    Improve default visualizers

--- HolonomicDrive.m
+++ HolonomicDrive.m
@@ -64,7 +64,7 @@ classdef HolonomicDrive < SecondOrderSystem
 					wheel = obj.wheels(i);
 					qdddxi = oneWheelGradients(obj.m, obj.I, wheel, t, q, qd, u(i));
 					qdddx(:,1:7) = qdddx(:,1:7) + qdddxi(:,1:7);
-					
+
 					% qdd / du_i
 					qdddx(:,7+i) = -qdddxi(:,7);
 				end
@@ -134,7 +134,9 @@ classdef HolonomicDrive < SecondOrderSystem
 	end
 
 	methods(Static)
-		function obj = defaultInstance(n)
+		function obj = unitModel(n)
+			% Construct a drive where all params are equal to 1, just like
+			% in the real world
 			r = 1;
 			if ~exist('n', 'var')
 				n = 3;
@@ -156,28 +158,94 @@ classdef HolonomicDrive < SecondOrderSystem
 			obj = HolonomicDrive(wheels, 1, 1);
 		end
 
+		function obj = plausibleModel()
+			% make some random guesses
+			wr = 0.025;
+			m = 1;
+
+			% taken from the C# code
+				robotToWheelRadius = 0.0783; % Distance from robot center to wheel center
+				wheelMSPerSpeed = 0.02952;   %Conversion from wheel speed [0,127] -> m/s
+				% When changing from the current speed to a new commanded speed, every 1 / (this number of seconds),
+				% the difference between the actual wheel speed and the commanded will be multiplied by 1/e
+				changeConstlf = 8;
+
+			% now try and decode these params
+			%  tau = u - b theta\dot
+			%  x\ddot = 1 / m * sum (\tau * dir / wr)
+			%  theta\ddot = 1 / (wr^2 m) sum (\tau * dir)
+			%             = 2 / (wr^2 m) * (umax - b theta\dot)
+			%  changeConstlf = 1 / (2 / (wr^2 m) * b)
+			% screw it. This is at least dimensionally correct:
+			wb = changeConstlf * (wr^2 * m);
+
+			speed_max = wheelMSPerSpeed * 127;
+			omega_max = speed_max / wr;
+			tau_max = wb * omega_max;
+
+			%
+			r = robotToWheelRadius;
+
+			% also a guess
+			I = 1/2 * m * r^2;
+
+			n = 4;
+			for i = 1:n
+				theta = 2*pi * (2*i-1)/(2*n);
+				wheels(i).pos = [r*cos(theta); r*sin(theta)];
+
+				% first column - active direction
+				% second column - passive direction
+				wheels(i).driveDir = [-sin(theta); cos(theta)];
+				wheels(i).slipDir = [cos(theta); sin(theta)];
+				wheels(i).b = wb;
+				wheels(i).r = wr;
+			end
+
+			obj = HolonomicDrive(wheels, I, m);
+			obj = setInputLimits(obj, -tau_max, tau_max);
+		end
+
 		function runTest()
-			plant = HolonomicDrive.defaultInstance();
+			plant = HolonomicDrive.plausibleModel();
+			% plant = extractTrigPolySystem(plant, struct('replace_output_w_new_state', true));
+
+
+			u0 = [1; -0.5;-1;0] .* plant.umax;
+			x0 = Point(plant.getStateFrame, zeros(plant.getNumOutputs, 1));
+
+			z = zeros(4, 1);
+			u = zoh([0 15 20], [u0,z,z]);
+			utraj = PPTrajectory(u);
+			utraj = setOutputFrame(utraj, plant.getInputFrame);
+			sys = cascade(utraj, plant);
+
+			xtraj = simulate(sys, [0 20], x0);
+
+			ts = 0:1/30:20;
+			xs = xtraj.eval(ts);
+			figure();
+			plot(ts, sqrt(xs(4,:).*xs(4,:) + xs(5,:).*xs(5,:)));
+
 			v = HolonomicDriveVisualizer(plant);
-
-
-			u0 = Point(plant.getInputFrame, [1; -1; 0]);
-			x0 = Point(plant.getStateFrame, zeros(6, 1));
-
-			sys = cascade(ConstantTrajectory(u0), plant);
-
-			xtraj = simulate(sys, [0 10], x0);
-
-			v.playback(xtraj, struct('slider', true));
+			function draw(t, x)
+				v.draw(t, x);
+				xs = xtraj.eval(xtraj.tspan(1):1/30:t);
+				plot(xs(1,:), xs(2,:));
+			end
+			v2 = FunctionHandleVisualizer(plant.getOutputFrame, @draw);
+			v2.playback(xtraj, struct('slider', true));
 		end
 
 		function trajectory()
-			plant = HolonomicDrive.defaultInstance(3);
+			plant = HolonomicDrive.unitModel(3);
 
 			x0 = zeros(6, 1);
 			xf = [0; 10; 0; 0; 0; 0];
 
 			[utraj, xtraj] = plant.optimalTrajectory(x0, xf, 15);
+
+			real_xtraj = simulate(cascade(utraj, plant), xtraj.tspan, x0);
 
 			t = utraj.tspan(1):0.1:utraj.tspan(2);
 			figure
@@ -188,6 +256,9 @@ classdef HolonomicDrive < SecondOrderSystem
 			plot(xs(1,:), xs(2,:));
 			quiver(xs(1,:), xs(2,:), cos(xs(3,:)), sin(xs(3,:)))
 
+
+			x2s = real_xtraj.eval(real_xtraj.tspan(1):0.1:real_xtraj.tspan(2));
+
 			v1 = HolonomicDriveVisualizer(plant);
 			function draw(t, x)
 				xs = xtraj.eval(xtraj.tspan(1):0.1:t);
@@ -197,7 +268,7 @@ classdef HolonomicDrive < SecondOrderSystem
 				v1.draw(t, x)
 			end
 			v2 = FunctionHandleVisualizer(plant.getOutputFrame, @draw);
-			
+
 			v2.playback(xtraj, struct('slider', true));
 		end
 	end
